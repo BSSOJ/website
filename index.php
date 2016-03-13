cb8318a99bebe109e0ea37642c496a95e4284daa
Henry Gu <guhenry3@gmail.com>
2016-03-13 17:39:29 -0400

    Added problem list

--- index.php
+++ index.php
@@ -3,6 +3,14 @@ session_start();
 require_once("config.php");
 
 $user = isset($_SESSION['user']) ? $_SESSION['user'] : null;
+
+$problems = [];
+$link = mysqli_connect($config['db']['addr'], $config['db']['user'], $config['db']['pass'], $config['db']['name'], $config['db']['port']);
+$result = mysqli_query($link, "SELECT name, code, SUM(testcases.score) AS score FROM problems JOIN testcases WHERE testcases.probid=problems.id GROUP BY problems.id");
+$i = 0;
+while($row = mysqli_fetch_assoc($result)){
+    $problems[$i++] = $row;
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,13 +27,29 @@ $user = isset($_SESSION['user']) ? $_SESSION['user'] : null;
             }
         ?>
         <form action="login.php" method="POST">
-        <input type="text" name="user" placeholder="Username"><input type="password" name="pass" placeholder="Password"><input type="submit">
+            <input type="text" name="user" placeholder="Username"><input type="password" name="pass" placeholder="Password"><input type="submit">
         </form>
         <?php
         }else{
             echo "You are logged in as, ". $user. " <a href=\"logout.php\">Logout</a>";
         }
         ?>
+        <table border="1">
+            <tr>
+                <th>Problem name</th>
+                <th>Problem code</th>
+                <th>Points value</th>
+            </tr>
+            <?php
+            foreach($problems as $problem){
+                echo "<tr>";
+                echo "<td>". $problem['name']. "</td>";
+                echo "<td>". $problem['code']. "</td>";
+                echo "<td>". $problem['score']. "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
     </body>
 </html>
 <?php
